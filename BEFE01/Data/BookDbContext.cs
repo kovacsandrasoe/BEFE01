04f1ecc35169043fac4c3439e3e52ec490304880
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2026-09-10 10:06:41 +0200

    dbcontext first version

---
+++ BEFE01/Data/BookDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BEFE01.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BEFE01.Data
+{
+    public class BookDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Person> People { get; set; }
+
+        public BookDbContext(DbContextOptions<BookDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
