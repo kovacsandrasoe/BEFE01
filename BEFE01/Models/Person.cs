f25fe8c9bcf61a8d66d36d13c066ec596ad2e8ac
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2026-09-10 09:46:58 +0200

    person added

---
+++ BEFE01/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BEFE01.Models
+{
+    public class Person
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Name { get; set; } = string.Empty;
+
+        public int Age { get; set; }
+
+    }
+}
