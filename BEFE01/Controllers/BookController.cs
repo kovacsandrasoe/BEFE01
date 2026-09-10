0f19233ec9839705eb96cbeae3ca23a6085ee21e
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2026-09-10 10:16:45 +0200

    context di

---
+++ BEFE01/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using BEFE01.Data;
+using BEFE01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BEFE01.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly BookDbContext _context;
+        public BookController(BookDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> GetBooks()
+        {
+            
+        }
+    }
+}
