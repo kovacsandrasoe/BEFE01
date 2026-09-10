ee9f36fdfdc3c5bc84e30988f744d99bc26af8c8
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2026-09-10 09:47:56 +0200

    book added

---
+++ BEFE01/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BEFE01.Models
+{
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Title { get; set; } = string.Empty;
+
+        public int Year { get; set; }
+
+    }
+}
