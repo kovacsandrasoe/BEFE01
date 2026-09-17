94ac995cae7bfe4f47ace895219271991aa58d82
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2026-09-17 10:08:21 +0200

    validationfilter added

---
+++ BEFE01/Tools/ValidationFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BEFE01.Tools
+{
+    public class ValidationFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid) 
+            {
+                var error = new ErrorModel(
+                    String.Join(',', context.ModelState.Values.SelectMany(t => 
+                    t.Errors.Select(z => z.ErrorMessage)).ToArray()
+                    ));
+                context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                context.Result = new JsonResult(error);
+            }
+
+        }
+    }
+}
